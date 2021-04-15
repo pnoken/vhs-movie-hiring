71455da6116e20d8c679e1ec5dcd88f90cac4f44
jayCde <68590879+jayCde@users.noreply.github.com>
2021-04-15 08:52:19 +0000

    Remove create-next-app defaults

--- pages/user/dashboard/index.js
+++
@@ -1,9 +0,0 @@
-import UserLayout from "../../../Component/Users/Layout";
-
-export default function UserProfile() {
-  return (
-    <UserLayout title="User Profile">
-      <h1>Welcoome to my profile</h1>
-    </UserLayout>
-  );
-}
