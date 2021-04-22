64fdb6a3331c83c11adbc56f9531e8b9d4487907
peter.nortey <peter.nortey@azubiafrica.org>
2021-04-22 03:30:04 +0000

    changed movie list

--- Component/Users/Layout/index.js
+++ Component/Users/Layout/index.js
@@ -12,10 +12,18 @@ export default function UserLayout({ title, children }) {
           integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
           crossOrigin="anonymous"
         ></link>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css"
+          integrity="sha384-wESLQ85D6gbsF459vf1CiZ2+rr+CsxRY0RpiF1tLlQpDnAgg6rwdsUF1+Ics2bni"
+          crossorigin="anonymous"
+        ></link>
       </Head>
       <body>
         <nav className="navbar navbar-dark bg-dark">
-        <Link href="/"><a className="text-light">VHS Movie Hiring</a></Link>
+          <Link href="/">
+            <a className="text-light">VHS Movie Hiring</a>
+          </Link>
         </nav>
         {children}
       </body>
--- pages/user/movies/index.js
+++ pages/user/movies/index.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 
 export default function UserProfile() {
   const [movies, setMovies] = useState([]);
-  const [ hired, setHired ] = useState(false);
+  const [hired, setHired] = useState(false);
 
   function getMovies() {
     var requestOptions = {
@@ -26,7 +26,7 @@ export default function UserProfile() {
     <UserLayout title="User Profile">
       <h2>Available Movies</h2>
 
-      <table className="table table-hover table-responsive">
+      {/* <table className="table table-hover table-responsive">
         <thead>
           <tr>
             <th>ID</th>
@@ -35,27 +35,51 @@ export default function UserProfile() {
             <th>Availability</th>
             <th>Action</th>
           </tr>
-        </thead>
+        </thead> */}
         {movies.map((movie) => (
-          <tbody key={movie.id}>
-            <tr id="d1">
-              <td>{movie.id}</td>
-              <td id="f1">{movie.name}</td>
-              <td id="l1">GHS {movie.price}</td>
-              <td id="m1">{movie.active ? "Available" : "Not Available"}</td>
-              <td>
-                {movie.active && !hired ? (
-                  <button  onClick={() => setHired(true)} className="btn btn-primary">hire</button>
-                ) : movie.active && hired ? (
-                  <button className="btn btn-success" disabled>hired</button>
-                ) : (
-                  <button className="btn btn-secondary" disabled>hire</button>
-                )}
-              </td>
-            </tr>
-          </tbody>
+          // <tbody key={movie.id}>
+          //   <tr id="d1">
+          //     <td>{movie.id}</td>
+          //     <td id="f1">{movie.name}</td>
+          //     <td id="l1">GHS {movie.price}</td>
+          //     <td id="m1">{movie.active ? "Available" : "Not Available"}</td>
+          //     <td>
+          //       {movie.active && !hired ? (
+          //         <button  onClick={() => setHired(true)} className="btn btn-primary">hire</button>
+          //       ) : movie.active && hired ? (
+          //         <button className="btn btn-success" disabled>hired</button>
+          //       ) : (
+          //         <button className="btn btn-secondary" disabled>hire</button>
+          //       )}
+          //     </td>
+          //   </tr>
+          // </tbody>
+          <div className="row">
+            <div className="col">
+          <div className="card movie_card">
+            <img
+              src="https://www.joblo.com/assets/images/joblo/posters/2019/02/captin-marvel-poster-international.jpg"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <i
+                className="fas fa-play play_button"
+                data-toggle="tooltip"
+                data-placement="bottom"
+                title="Play Trailer"
+              ></i>
+              <h5 className="card-title">{movie.name}</h5>
+              <span className="movie_info">{movie.release_year}</span>
+              <span className="movie_info float-right">
+                <i className="fas fa-star"></i>{movie.rating}
+              </span>
+            </div>
+          </div>
+          </div>
+          </div>
         ))}
-      </table>
+      {/* </table> */}
     </UserLayout>
   );
 }
