c37064fe1e38bf7d19ea6aa879ff803ff197108b
peter.nortey <peter.nortey@azubiafrica.org>
2021-04-23 07:06:20 +0000

    added hire button

--- pages/user/movies/index.js
+++ pages/user/movies/index.js
@@ -24,7 +24,7 @@ export default function UserProfile() {
 
   return (
     <UserLayout title="User Profile">
-      <h2>Available Movies</h2>
+      <h2>Movie List</h2>
 
       {/* <table className="table table-hover table-responsive">
         <thead>
@@ -36,6 +36,7 @@ export default function UserProfile() {
             <th>Action</th>
           </tr>
         </thead> */}
+      <div className="row">
         {movies.map((movie) => (
           // <tbody key={movie.id}>
           //   <tr id="d1">
@@ -54,31 +55,50 @@ export default function UserProfile() {
           //     </td>
           //   </tr>
           // </tbody>
-          <div className="row">
-            <div className="col">
-          <div className="card movie_card">
-            <img
-              src="https://www.joblo.com/assets/images/joblo/posters/2019/02/captin-marvel-poster-international.jpg"
-              className="card-img-top"
-              alt="..."
-            />
-            <div className="card-body">
-              <i
-                className="fas fa-play play_button"
-                data-toggle="tooltip"
-                data-placement="bottom"
-                title="Play Trailer"
-              ></i>
-              <h5 className="card-title">{movie.name}</h5>
-              <span className="movie_info">{movie.release_year}</span>
-              <span className="movie_info float-right">
-                <i className="fas fa-star"></i>{movie.rating}
-              </span>
+
+          <div className="col">
+            <div className="card movie_card">
+              <img
+                src="https://www.joblo.com/assets/images/joblo/posters/2019/02/captin-marvel-poster-international.jpg"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <i
+                  className="fas fa-play play_button"
+                  data-toggle="tooltip"
+                  data-placement="bottom"
+                  title="Play Trailer"
+                ></i>
+                <h5 className="card-title">{movie.name}</h5>
+                {movie.active && !hired ? (
+                  <button
+                    onClick={() => setHired(true)}
+                    className="btn btn-primary info"
+                  >
+                    hire
+                    <span className="extra-info">Available for hiring</span>
+                  </button>
+                ) : movie.active && hired ? (
+                  <button className="btn btn-success" disabled>
+                    hired
+                  </button>
+                ) : (
+                  <button className="btn btn-secondary info" disabled>
+                    hire
+                    <span className="extra-info">Unavailable for hiring</span>
+                  </button>
+                )}
+                <span className="movie_info">{movie.release_year}</span>
+                <span className="movie_info float-right">
+                  <i className="fas fa-star"></i>
+                  {movie.rating}
+                </span>
+              </div>
             </div>
           </div>
-          </div>
-          </div>
         ))}
+      </div>
       {/* </table> */}
     </UserLayout>
   );
