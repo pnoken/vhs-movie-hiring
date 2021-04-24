84895966bd41a50e7232df0dc8efcc18c26f366f
jayCde <68590879+jayCde@users.noreply.github.com>
2021-04-24 05:27:47 +0000

    Add main component to render userdashboard

---
+++ Component/Users/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import UsersDashboard from './dashboard'
+import HeaderElement from './header'
+
+
+
+const MainCanvas = () => {
+    return (
+        <div>
+            <UsersDashboard/>
+        </div>
+    )
+}
+
+export default MainCanvas
