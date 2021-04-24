c2ddf9d31ee8c019a55d15590bd87332a6101fb8
jayCde <68590879+jayCde@users.noreply.github.com>
2021-04-24 05:27:15 +0000

    Add footer

---
+++ Component/Users/footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from '../../../styles/footer.module.css'
+
+const FooterElement = () => {
+    return (
+        <div>
+            <footer className={styles.footer}>
+                <div>
+                    &#169; VHS - Movie Hiring.
+                </div>
+                <div>
+                    <p>&#174; The JavScript Learning Group <sup>&#8482;</sup></p>
+                </div>
+        
+                Powered by{' '}
+                <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+            </footer>     
+        </div>
+    )
+}
+
+export default FooterElement
