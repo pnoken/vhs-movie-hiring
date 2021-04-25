a4f9b53d8ebda1ab638cab995f731c5c97f2ee13
clemsedem <clemsedem@gmail.com>
2021-04-25 23:17:27 +0000

    admin add users

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery';
+import ModalContext from '../Contexts/ModalContext';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	const [showModal, setShowModal] = useState(false);
+	const closeModal = () => {
+		setShowModal(false);
+	};
+	const modalValues = {
+		show: showModal,
+		setShow: setShowModal,
+		close: closeModal,
+	};
+
+	return (
+		<ModalContext.Provider value={modalValues}>
+			<Component {...pageProps} />
+		</ModalContext.Provider>
+	);
 }
 
-export default MyApp
+export default MyApp;
