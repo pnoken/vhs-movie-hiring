26f1cfdd43b378bdf4b3d83264449e94c65467ac
peter.nortey <peter.nortey@azubiafrica.org>
2021-04-26 08:02:06 +0000

    merged changes

--- Component/Users/Dashboard/Wallet/wallet.jsx
+++ Component/Users/Dashboard/Wallet/wallet.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Head from "next/head";
 // import Body from 'next/body'
-import HeaderElement from "../header";
-import FooterElement from "../footer";
+import HeaderElement from "../../header";
+import FooterElement from "../../footer";
 import axios from "axios";
 import styles from "../../../../styles/users/wallet.module.css";
 
