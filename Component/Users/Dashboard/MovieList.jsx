0ce328bba32b25a5f3112cd8a35a892107576291
jayCde <68590879+jayCde@users.noreply.github.com>
2021-04-27 16:52:43 +0000

    Format documents

--- Component/Users/Dashboard/MovieList.jsx
+++ Component/Users/Dashboard/MovieList.jsx
@@ -1,26 +1,24 @@
 import React, { useState, useEffect } from "react";
 import styles from "../../../styles/users/generalmovielist.module.css";
 
-
 export default function MovieList() {
-
   //define styles
-  const {movielist, movieGrid, movieCard, moviePoster} = styles;
+  const { movielist, movieGrid, movieCard, moviePoster } = styles;
 
   const [movies, setMovies] = useState([]);
   const [hired, setHired] = useState(false);
 
   function getMovies() {
     var requestOptions = {
-      method: "GET",
+      method: "GET"
     };
 
     fetch("http://localhost:7000/movies", requestOptions)
-      .then((response) => response.json())
-      .then((result) => {
+      .then(response => response.json())
+      .then(result => {
         setMovies(result), console.log(result);
       })
-      .catch((error) => console.log("error", error));
+      .catch(error => console.log("error", error));
   }
 
   useEffect(() => {
@@ -29,23 +27,39 @@ export default function MovieList() {
 
   return (
     <div>
-      <div  style={{display: "flex", width: "100%", marginTop: "5%", backgroundColor: "rgb(47, 44, 44)", flexWrap: "wrap"}}>
-  
-        {movies.map((movie) => (
-          <div style={{flex: "1 0 20%", margin:"5px", height: "30%"}}>
-            <div className="card movie_card" style={{width: "94%", borderRadius:"8px", padding: "2px 2px 2px 2px"}}>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          marginTop: "5%",
+          backgroundColor: "rgb(47, 44, 44)",
+          flexWrap: "wrap"
+        }}
+      >
+        {movies.map(movie => (
+          <div style={{ flex: "1 0 20%", margin: "5px", height: "30%" }}>
+            <div
+              className="card movie_card"
+              style={{
+                width: "94%",
+                borderRadius: "8px",
+                padding: "2px 2px 2px 2px"
+              }}
+            >
               <img
                 src="https://www.joblo.com/assets/images/joblo/posters/2019/02/captin-marvel-poster-international.jpg"
-                style={{width: "300px", height: "360px", borderRadius: "8px"}}
+                style={{ width: "300px", height: "360px", borderRadius: "8px" }}
                 alt="..."
               />
-              <div  >
-                <p><i
-                  className="fas fa-play play_button"
-                  data-toggle="tooltip"
-                  data-placement="bottom"
-                  title="Play Trailer"
-                ></i></p>
+              <div>
+                <p>
+                  <i
+                    className="fas fa-play play_button"
+                    data-toggle="tooltip"
+                    data-placement="bottom"
+                    title="Play Trailer"
+                  ></i>
+                </p>
                 <h6 className="card-title">{movie.name}</h6>
                 {movie.active && !hired ? (
                   <button
@@ -65,11 +79,17 @@ export default function MovieList() {
                     <span className="extra-info">Unavailable for hiring</span>
                   </button>
                 )}
-                <p><span className="movie_info float-left">{movie.release_year}</span>{" "}
-                <p><span className="movie_info float-right">
-                  <i className="fas fa-star"></i>
-                  {movie.rating}
-                </span></p></p>
+                <p>
+                  <span className="movie_info float-left">
+                    {movie.release_year}
+                  </span>{" "}
+                  <p>
+                    <span className="movie_info float-right">
+                      <i className="fas fa-star"></i>
+                      {movie.rating}
+                    </span>
+                  </p>
+                </p>
               </div>
             </div>
           </div>
--- Component/Users/dashboard.jsx
+++ Component/Users/dashboard.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 
-import HeaderElement from './header';
-import FooterElement from './footer'
-import MovieSlideshow from './slideshow'
+import HeaderElement from "./header";
+import FooterElement from "./footer";
+import MovieSlideshow from "./slideshow";
 // import classNames from "classnames/bind";
-import axios from 'axios'
+import axios from "axios";
 import styles from "../../styles/users/userdashboard.module.css";
-import MovieList from './Dashboard/MovieList';
+import MovieList from "./Dashboard/MovieList";
 
 const UsersDashboard = () => {
   const {
@@ -124,7 +124,7 @@ const UsersDashboard = () => {
 
   function handleClick(e, id) {
     e.preventDefault();
-    id
+    id;
     setAddToCart(prevAddToCart => (prevAddToCart = true));
   }
 
@@ -132,7 +132,10 @@ const UsersDashboard = () => {
     <div>
       <Head>
         <title>VHS Movie Hiring | Dashboard</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
         <script
           src="https://kit.fontawesome.com/a076d05399.js"
           crossOrigin="anonymous"
@@ -143,12 +146,19 @@ const UsersDashboard = () => {
         <div className={upperDashboardSection}>
           <div className={menu}>
             <div className={items}>
-              <img src="/img/vhs-logo.png" alt="logo" style={{float: "left", margin: "12px",
+              <img
+                src="/img/vhs-logo.png"
+                alt="logo"
+                style={{
+                  float: "left",
+                  margin: "12px",
                   marginTop: "1%",
                   marginBottom: "5%",
                   float: "center",
                   height: "32px",
-                  width: "48px"}}/>
+                  width: "48px"
+                }}
+              />
 
               <ul>
                 <li>
@@ -181,7 +191,9 @@ const UsersDashboard = () => {
                 <li>
                   <a href="/">
                     {" "}
-                    <strong>Logout <span>&#8618;</span></strong>
+                    <strong>
+                      Logout <span>&#8618;</span>
+                    </strong>
                   </a>
                 </li>
               </ul>
@@ -191,117 +203,143 @@ const UsersDashboard = () => {
         {/* <MovieSlideshow/> */}
       </div>
 
-<hr style={{marginBottom:"10px"}}/>
+      <hr style={{ marginBottom: "10px" }} />
 
-        {/* Movie Packages - 3 Packages */}
+      {/* Movie Packages - 3 Packages */}
 
-        <div>
-            <div className={leftbox} id = "leftbox">
-                    <h3>BlockBuster Package</h3>
+      <div>
+        <div className={leftbox} id="leftbox">
+          <h3>BlockBuster Package</h3>
 
-                    {movies.map((movies, id) => {
-                        return (
-                        <div key={movies.id}>
-                            
-                            <div >
-                                <img src={movies.poster} alt="image" style={{width: "350px", height:"350px", borderRadius:"10px", boxShadow:"1px 1px 1px 1px white"}}/>
-                            </div>
-                            <div className={detail}>
-                                <div className={title}>{movies.movie_title} </div>
-                                <div className={desc}>{movies.movie_description}</div>
-                                <div className={price}>${movies.price} {MoviePrice}</div>
-                                <div>Total Price {totalPrice}$</div>
-                            </div>
-                            <div className={(setquan, setbg)}>
-                                <div className={sQuantity}>Duration (in days) {quantity}</div>
-                                <div className={btns}>
-                                    <button className={btn} onClick={incrementQuentaty}>
-                                    +
-                                    </button>
-                                    <button className={btn} onClick={decrementQuantity}>
-                                    -
-                                    </button>
-                                </div>
-                            </div>
-                           
-                            <div className={(add, setbg)}>
-                                <button className={btn} onClick={handleClick}>
-                                Add to Cart
-                                </button>
-                            </div>
-
-                        </div>
-                        );
-                    })}
-            </div> 
-              
-            <div className={middlebox} id = "middlebox">
-                <h3>Si-Fi Package</h3>
-                <div >
-                        <img src="/img/movie-poster-after.jpg" alt="image" style={{width: "350px", height:"350px", borderRadius:"10px", boxShadow:"2px 2px 2px 2px"}}/>
-                    </div>
-                    <div className={detail}>
-                        <div className={title}>Movie Title: After</div>
-                        <div className={desc}>Movie Description</div>
-                    </div>
-                    <div className={(setquan, setbg)}>
-                        <div className={sQuantity}>Duration (in days) {quantity}</div>
-                        <div className={btns}>
-                            <button className={btn} onClick={incrementQuentaty}>
-                            +
-                            </button>
-                            <button className={btn} onClick={decrementQuantity}>
-                            -
-                            </button>
-                        </div>
-                    </div>
-                    <div className={price}>
-                        <div> Package Price {MoviePrice}$</div>
-                    <div>Total Price {totalPrice}$</div>
+          {movies.map((movies, id) => {
+            return (
+              <div key={movies.id}>
+                <div>
+                  <img
+                    src={movies.poster}
+                    alt="image"
+                    style={{
+                      width: "350px",
+                      height: "350px",
+                      borderRadius: "10px",
+                      boxShadow: "1px 1px 1px 1px white"
+                    }}
+                  />
                 </div>
+                <div className={detail}>
+                  <div className={title}>{movies.movie_title} </div>
+                  <div className={desc}>{movies.movie_description}</div>
+                  <div className={price}>
+                    ${movies.price} {MoviePrice}
+                  </div>
+                  <div>Total Price {totalPrice}$</div>
+                </div>
+                <div className={(setquan, setbg)}>
+                  <div className={sQuantity}>Duration (in days) {quantity}</div>
+                  <div className={btns}>
+                    <button className={btn} onClick={incrementQuentaty}>
+                      +
+                    </button>
+                    <button className={btn} onClick={decrementQuantity}>
+                      -
+                    </button>
+                  </div>
+                </div>
+
                 <div className={(add, setbg)}>
-                <button className={btn} onClick={handleClick}>
+                  <button className={btn} onClick={handleClick}>
                     Add to Cart
-                </button>
+                  </button>
                 </div>
-            </div>
-              
-            <div className={rightbox} id = "rightbox">
-                <h3>Comedy Package</h3>
-                <div >
-                        <img src="/img/movie-poster-after.jpg" alt="image" style={{width: "350px", height:"350px", borderRadius:"10px", boxShadow:"1px 1px 1px 1px white"}}/>
-                    </div>
-                    <div className={detail}>
-                        <div className={title}>Movie Title: After</div>
-                        <div className={desc}>Movie Description</div>
-                    </div>
-                    <div className={(setquan, setbg)}>
-                        <div className={sQuantity}>Duration (in days) {quantity}</div>
-                        <div className={btns}>
-                            <button className={btn} onClick={incrementQuentaty}>
-                            +
-                            </button>
-                            <button className={btn} onClick={decrementQuantity}>
-                            -
-                            </button>
-                        </div>
-                    </div>
-                    <div className={price}>
-                        <div> Package Price {MoviePrice}$</div>
-                    <div>Total Price {totalPrice}$</div>
-                </div>
-                <div className={(add, setbg)}>
-                <button className={btn} onClick={handleClick}>
-                    Add to Cart
-                </button>
-                </div>
-            </div>
-
+              </div>
+            );
+          })}
         </div>
 
-        <div>
-            <MovieList />
+        <div className={middlebox} id="middlebox">
+          <h3>Si-Fi Package</h3>
+          <div>
+            <img
+              src="/img/movie-poster-after.jpg"
+              alt="image"
+              style={{
+                width: "350px",
+                height: "350px",
+                borderRadius: "10px",
+                boxShadow: "2px 2px 2px 2px"
+              }}
+            />
+          </div>
+          <div className={detail}>
+            <div className={title}>Movie Title: After</div>
+            <div className={desc}>Movie Description</div>
+          </div>
+          <div className={(setquan, setbg)}>
+            <div className={sQuantity}>Duration (in days) {quantity}</div>
+            <div className={btns}>
+              <button className={btn} onClick={incrementQuentaty}>
+                +
+              </button>
+              <button className={btn} onClick={decrementQuantity}>
+                -
+              </button>
+            </div>
+          </div>
+          <div className={price}>
+            <div> Package Price {MoviePrice}$</div>
+            <div>Total Price {totalPrice}$</div>
+          </div>
+          <div className={(add, setbg)}>
+            <button className={btn} onClick={handleClick}>
+              Add to Cart
+            </button>
+          </div>
         </div>
+
+        <div className={rightbox} id="rightbox">
+          <h3>Comedy Package</h3>
+          <div>
+            <img
+              src="/img/movie-poster-after.jpg"
+              alt="image"
+              style={{
+                width: "350px",
+                height: "350px",
+                borderRadius: "10px",
+                boxShadow: "1px 1px 1px 1px white"
+              }}
+            />
+          </div>
+          <div className={detail}>
+            <div className={title}>Movie Title: After</div>
+            <div className={desc}>Movie Description</div>
+          </div>
+          <div className={(setquan, setbg)}>
+            <div className={sQuantity}>Duration (in days) {quantity}</div>
+            <div className={btns}>
+              <button className={btn} onClick={incrementQuentaty}>
+                +
+              </button>
+              <button className={btn} onClick={decrementQuantity}>
+                -
+              </button>
+            </div>
+          </div>
+          <div className={price}>
+            <div> Package Price {MoviePrice}$</div>
+            <div>Total Price {totalPrice}$</div>
+          </div>
+          <div className={(add, setbg)}>
+            <button className={btn} onClick={handleClick}>
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <MovieList />
+      </div>
     </div>
   );
 };
