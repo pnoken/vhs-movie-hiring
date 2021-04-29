92d02a84b757cc6fded38653e0768856cb2ebcef
jayCde <68590879+jayCde@users.noreply.github.com>
2021-04-29 12:26:55 +0000

    remove Head imports

--- Component/Users/Dashboard/Wallet/wallet.jsx
+++ Component/Users/Dashboard/Wallet/wallet.jsx
@@ -106,14 +106,6 @@ const MyWallet = () => {
         <title>Wallet | Virtual Card</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-        />
-        <script
-          src="https://kit.fontawesome.com/a076d05399.js"
-          crossOrigin="anonymous"
-        ></script>
       </Head>
 
       <div className={body}>
