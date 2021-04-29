bc4f34bed7efe76998e516259547206111bf7c8c
jayCde <68590879+jayCde@users.noreply.github.com>
2021-04-29 15:31:37 +0000

    Modify wallet functionalities

--- Component/Users/Dashboard/Wallet/wallet.jsx
+++ Component/Users/Dashboard/Wallet/wallet.jsx
@@ -28,11 +28,9 @@ const MyWallet = () => {
   } = styles;
 
   //function to top-up user wallet
-
-  useEffect(() => {
-    const TopupWallet = async () => {
+    const TopupWallet = async (data={balance}) => {
       const response = await axios
-        .get(`https://hiring-vhs.herokuapp.com/users/wallet`, {
+        .patch(`http://localhost:7000/users.wallet?${data.balance}`, {
           headers: {
             "Content-Type": "application/json",
             Accept: "application/json"
@@ -53,43 +51,66 @@ const MyWallet = () => {
           );
         });
     };
-    TopupWallet();
-  }, []);
+
 
   //--function to fetch user details from localStorage--//
   const [id, setID] = useState("");
-  const [first_name, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [username, setUsername] = useState("");
+  const [firstName, setFirstname] = useState("");
+  const [lastName, setLastname] = useState("");
+  const [userName, setUsername] = useState("");
   const [wallet, setWallet] = useState({});
   const [wallet_id, setWalletid] = useState("");
   const [currency, setCurrency] = useState();
   const [balance, setBalance] = useState("");
 
-  useEffect(() => {
-    // const getUserDetails = () =>{
-    const userdetails = localStorage.getItem("user");
-    if (userdetails) {
-      const usercredentials = JSON.parse(userdetails);
-      setID(usercredentials.id);
-      setFirstname(usercredentials.first_name);
-      setLastname(usercredentials.lastname);
-      setUsername(usercredentials.username);
-      setWallet(usercredentials.wallet);
-      setWalletid(usercredentials.wallet.wallet_id);
-      setCurrency(usercredentials.wallet.currency);
-      setBalance(usercredentials.wallet.balance);
-
-      console.log(usercredentials);
+  useEffect(()=>{
+    const userdata = localStorage.getItem('user-credentials');
+    
+    if(userdata) {
+        const foundUser = JSON.parse(userdata)
+        setID(foundUser.id);
+        setFirstname(foundUser.firstName);
+        setLastname(foundUser.lastName);
+        setUsername(foundUser.userName);
+        setCurrency(foundUser.currency);
+        // setWallet(usercredentials.wallet);
+        console.log('User is logged in')
     } else {
-      console.log(
-        "User credentials unavailable. Your session might have timed out"
-      );
-      // alert("User details credentials unavailable. Your session might have timed out");
+        window.location="/"
     }
-    // }
-    // userdetails();
-  }, []);
+
+  }, [])
+  
+  // useEffect(() => {
+  //   // const getUserDetails = () =>{
+  //   const userdetails = localStorage.getItem("user-credentials");
+  //   if (userdetails) {
+  //     const usercredentials = JSON.parse(userdetails);
+  //     setID(usercredentials.id);
+  //     setFirstname(usercredentials.firstName);
+  //     setLastname(usercredentials.lastName);
+  //     setUsername(usercredentials.usernName);
+  //     setWallet(usercredentials.wallet);
+  //     // setWalletid(usercredentials.wallet.wallet_id);
+  //     // setCurrency(usercredentials.wallet.currency);
+  //     // setBalance(usercredentials.wallet.balance);
+
+  //     console.log(usercredentials);
+  //   } else {
+  //     console.log(
+  //       "User credentials unavailable. Your session might have timed out"
+  //     );
+  //     // alert("User details credentials unavailable. Your session might have timed out");
+  //   }
+  //   // }
+  //   // userdetails();
+  // }, []);
+
+  const [totalBalance, settotalBalance] = useState(balance);
+
+  function newuserBalance () {
+      settotalBalance(prevBalance => prevBalance + balance);
+    }
 
   //other constant variables
   const cardType = "VHS Virtual Card";
@@ -117,21 +138,21 @@ const MyWallet = () => {
         <div className={container}>
           <div className={virtualCard} id="virtual-card">
             <label className={virtualCardBalance} id="virtual-card-balance">
-              Balance: {currency} {balance}
+              Balance: {currency} {" "} {totalBalance}
             </label>
             <label className={virtualCardField} id="card-type">
               Type: {cardType}
             </label>
             <br />
 
-            <label className={virtualCardField}>ID: {wallet_id}</label>
+            <label className={virtualCardField}>ID: {id} {userName}</label>
             <br />
             <label className={virtualCardField} id="user-name">
-              Card Holder: {first_name} {lastname}
+              Card Holder: {firstName} {" "} {lastName}
             </label>
             <br />
             <label className={virtualCardField} id="date-isu">
-              Expiry: {cardExpiry}{" "}
+              Expiry: {" "} {cardExpiry}
             </label>
 
             <img
@@ -192,7 +213,8 @@ const MyWallet = () => {
                 type="submit"
                 className={topupBtn}
                 id="topup-btn"
-                value="Top-up"
+                value="submit"
+                onClick={TopupWallet, newuserBalance}
               />
             </form>
           </div>
