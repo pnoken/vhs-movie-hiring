2bc15fec20f205b721d2ebeea89c62eb46cdd0da
peter.nortey <peter.nortey@azubiafrica.org>
2021-06-08 11:05:05 +0000

    corrected import for validateinfo

--- components/topup/useForm.js
+++ components/topup/useForm.js
@@ -1,40 +1,40 @@
-import { useState } from 'react'
-import validateInfo from './validateInfo';
+import { useState } from 'react';
+import validateInfo from './ValidateInfo';
 
 const useForm = () => {
-    const [values, setValues] = useState({
-        cardName: '',
-        cardNumber: '',
-        cardType: '',
-        cardExpiration: '',
-        cardSecurityCode: '',
-        cardPostalCode: '',
-        focus: ''
-    })
+  const [values, setValues] = useState({
+    cardName: '',
+    cardNumber: '',
+    cardType: '',
+    cardExpiration: '',
+    cardSecurityCode: '',
+    cardPostalCode: '',
+    focus: '',
+  });
 
-    const [errors, setErrors] = useState({})
+  const [errors, setErrors] = useState({});
 
-    const handleFocus = (e) => {
-        setValues({ 
-            ...values,
-            focus: (e.target.name === 'cardSecurityCode') ? 'cvc' : e.target.name
-        });
-    }
+  const handleFocus = e => {
+    setValues({
+      ...values,
+      focus: e.target.name === 'cardSecurityCode' ? 'cvc' : e.target.name,
+    });
+  };
 
-    const handleChange = e => {
-        const { name, value } = e.target
-        setValues({
-            ...values,
-            [name]: value
-        })
-    }
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
 
-    const handleSubmit = e => {
-        e.preventDefault()
-        setErrors(validateInfo(values))
-    };
-    
-    return { handleChange, handleFocus, handleSubmit, values, errors };
+  const handleSubmit = e => {
+    e.preventDefault();
+    setErrors(validateInfo(values));
+  };
+
+  return { handleChange, handleFocus, handleSubmit, values, errors };
 };
 
-export default useForm; 
+export default useForm;
